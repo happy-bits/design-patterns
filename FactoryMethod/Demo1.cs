5a22e40bb3b7887613128443219f24ec1155f668
Oscar Olsson <happybits@gmail.com>
2020-07-13 09:39:16 +0200

    Factory method pattern - two examples - please comment :)

---
+++ FactoryMethod/Demo1.cs
@@ -0,0 +1,133 @@
+﻿/*
+ 
+FACTORY METHOD - design pattern
+
+Two types of "DocumentCreator":
+- HtmlCreator
+- MarkdownCreator
+
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.FactoryMethod
+{
+    [TestClass]
+    public class Demo1
+    {
+        static readonly List<string> _log = new List<string>();
+
+        [TestMethod]
+        public void Ex1()
+        {
+            var c = new Client();
+
+            c.ClientCode(new HtmlCreator());
+            c.ClientCode(new MarkdownCreator());
+
+            CollectionAssert.AreEqual(new[] {
+                "<h1>HeaderA</h1>\n<body>BodyA</body>",
+                "<h1>HeaderB</h1>\n<body>BodyB</body>",
+                "<body>BodyC</body>\n<h1>HeaderC</h1>",
+
+                "# HeaderA\nBodyA",
+                "# HeaderB\nBodyB",
+                "BodyC\n# HeaderC",
+            }, _log);
+        }
+
+        // "Creator"
+        abstract class DocumentCreator
+        {
+            public bool Ascending { get; set; } = true;
+            public string Delimiter { get; set; } = "\n";
+
+            internal abstract IEnumerable<Element> Create(string header, string body);
+
+            internal string Render(string header, string body)
+            {
+                var elements = Create(header, body);
+
+                if (!Ascending)
+                    elements.Reverse();
+
+                return string.Join(Delimiter, elements.Select(e => e.Render()));
+            }
+
+        }
+
+        // "Concrete Creator" 
+        class HtmlCreator : DocumentCreator
+        {
+            internal override IEnumerable<Element> Create(string header, string body)
+            {
+                yield return new HtmlHeader(header);
+                yield return new HtmlBody(body);
+            }
+        }
+
+        // "Concrete Creator" 
+        class MarkdownCreator : DocumentCreator
+        {
+            internal override IEnumerable<Element> Create(string header, string body)
+            {
+                yield return new MarkdownHeader(header);
+                yield return new MarkdownBody(body);
+            }
+        }
+
+        // "Product"
+        abstract class Element
+        {
+            public Element(string content) => Content = content;
+            public string Content { get; }
+            public abstract string Render();
+        }
+
+        // "Concrete Product"
+        class HtmlHeader : Element
+        {
+            public HtmlHeader(string content) : base(content) { }
+            public override string Render() => $"<h1>{Content}</h1>";
+        }
+
+        // "Concrete Product"
+        class HtmlBody : Element
+        {
+            public HtmlBody(string content) : base(content) { }
+            public override string Render() => $"<body>{Content}</body>";
+        }
+
+        // "Concrete Product"
+        class MarkdownHeader : Element
+        {
+            public MarkdownHeader(string content) : base(content) { }
+            public override string Render() => $"# {Content}";
+        }
+
+        // "Concrete Product"
+        class MarkdownBody : Element
+        {
+            public MarkdownBody(string content) : base(content) { }
+            public override string Render() => Content;
+        }
+
+        // "Client"
+        class Client
+        {
+            public void ClientCode(DocumentCreator creator)
+            {
+                string result1 = creator.Render("HeaderA", "BodyA");
+                string result2 = creator.Render("HeaderB", "BodyB");
+
+                creator.Ascending = false;
+                string result3 = creator.Render("HeaderC", "BodyC");
+
+                _log.Add(result1);
+                _log.Add(result2);
+                _log.Add(result3);
+            }
+        }
+    }
+}
---
+++ FactoryMethod/Demo2.cs
@@ -0,0 +1,115 @@
+﻿/*
+ 
+FACTORY METHOD - design pattern
+
+Two types of products:
+- DetailedLogger
+- SimpleLogger
+
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DesignPatterns.FactoryMethod
+{
+    [TestClass]
+    public class Demo2
+    {
+        // (this field has nothing to do with the class Log below)
+        static readonly List<string> _log = new List<string>();
+
+        [TestMethod]
+        public void Ex1()
+        {
+            var c = new Client();
+
+            c.ClientCode(new DetailedLoggerCreator());
+            c.ClientCode(new SimpleLoggerCreator());
+
+            CollectionAssert.AreEqual(new[] {
+                "Detailed logger writes info message 'MessageA'",
+                "Detailed logger writes info message 'MessageB'",
+
+                "Simple logger writes info message 'MessageA'",
+                "Simple logger writes info message 'MessageB'",
+            }, _log);
+        }
+
+        // "Creator"
+        abstract class LoggerCreator
+        {
+            private Log _log;
+
+            public abstract Log CreateLogger();
+
+            public bool IsActive { get; set; }
+
+            public void Info(string message)
+            {
+                if (!IsActive)
+                    return;
+
+                _log ??= CreateLogger();    // (compound assignment)
+
+                _log.Info(message);
+            }
+
+        }
+
+        // "Concrete Creator" 
+        class DetailedLoggerCreator : LoggerCreator
+        {
+            public override Log CreateLogger()
+            {
+                return new DetailedLogger();
+            }
+        }
+
+        // "Concrete Creator" 
+        class SimpleLoggerCreator : LoggerCreator
+        {
+            public override Log CreateLogger()
+            {
+                return new SimpleLogger();
+            }
+        }
+
+        abstract class Log
+        {
+            public abstract void Info(string message);
+        }
+
+        // "Concrete Product"
+        class DetailedLogger : Log
+        {
+            public override void Info(string message)
+            {
+                _log.Add($"Detailed logger writes info message '{message}'");
+            }
+        }
+
+        // "Concrete Product"
+        class SimpleLogger : Log
+        {
+            public override void Info(string message)
+            {
+                _log.Add($"Simple logger writes info message '{message}'");
+            }
+        }
+
+        // "Client"
+        class Client
+        {
+            public void ClientCode(LoggerCreator creator)
+            {
+                creator.IsActive = true;
+                creator.Info("MessageA");
+                creator.Info("MessageB");
+
+                creator.IsActive = false;
+                creator.Info("MessageC");
+            }
+        }
+    }
+}
