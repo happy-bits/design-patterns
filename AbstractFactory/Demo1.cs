62f5a72c4a83361b7f3adcb6f08be9899d5dddda
Oscar Olsson <happybits@gmail.com>
2020-07-13 16:26:19 +0200

    Abstract Factory

---
+++ AbstractFactory/Demo1.cs
@@ -0,0 +1,168 @@
+﻿/*
+Conceptual example from Refactoring Guru 
+https://refactoring.guru/design-patterns/abstract-factory/csharp/example
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AbstractFactory
+{
+    [TestClass]
+    public class Demo1
+    {
+        [TestMethod]
+        public void Ex2()
+        {
+            new Client().Main();
+        }
+
+
+        public interface IAbstractFactory
+        {
+            IAbstractProductA CreateProductA();
+            IAbstractProductB CreateProductB();
+        }
+
+        /*
+
+           Konkreta factories producerar en familj av produkter. En familj kan ha flera varianter, 
+           men produkterna av en familj är inte kompatibla med produkter i andra familjer
+
+           IAbstractProductA kan vara en stol och ConcreteProductA1 en viktoriansk stol
+
+           "Viktoriansk fabrik"
+        */
+        class ConcreteFactory1 : IAbstractFactory
+        {
+            // "Viktoriansk stol"
+            public IAbstractProductA CreateProductA()
+            {
+                return new ConcreteProductA1();
+            }
+
+            // "Viktorianskt bord"
+            public IAbstractProductB CreateProductB()
+            {
+                return new ConcreteProductB1();
+            }
+        }
+
+        /*
+           En till fabrik (som t.ex tillverkar moderna möbler eller art-deco-möbler
+           "Art-deco fabrik"
+        */
+        class ConcreteFactory2 : IAbstractFactory
+        {
+            // "Art-deco stol"
+            public IAbstractProductA CreateProductA()
+            {
+                return new ConcreteProductA2();
+            }
+
+            // "Art-deco bord"
+            public IAbstractProductB CreateProductB()
+            {
+                return new ConcreteProductB2();
+            }
+        }
+
+        // En förmåga som alla stolar har
+        public interface IAbstractProductA
+        {
+            string UsefulFunctionA();
+        }
+
+        class ConcreteProductA1 : IAbstractProductA
+        {
+            public string UsefulFunctionA()
+            {
+                return "The result of the product A1.";
+            }
+        }
+
+        class ConcreteProductA2 : IAbstractProductA
+        {
+            public string UsefulFunctionA()
+            {
+                return "The result of the product A2.";
+            }
+        }
+
+        // En förmåga som alla bord har (de kan också samarbeta med stolarna)
+        public interface IAbstractProductB
+        {
+            // Product B kan gör sin egen grej
+            string UsefulFunctionB();
+
+            // ...men den kan också samarbeta med ProductA.
+            
+            string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+        }
+
+        class ConcreteProductB1 : IAbstractProductB
+        {
+            public string UsefulFunctionB()
+            {
+                return "The result of the product B1.";
+            }
+
+            public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+            {
+                var result = collaborator.UsefulFunctionA();
+
+                return $"The result of the B1 collaborating with the ({result})";
+            }
+        }
+
+        class ConcreteProductB2 : IAbstractProductB
+        {
+            public string UsefulFunctionB()
+            {
+                return "The result of the product B2.";
+            }
+
+            public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+            {
+                var result = collaborator.UsefulFunctionA();
+
+                return $"The result of the B2 collaborating with the ({result})";
+            }
+        }
+
+        /*
+         Klienten kan enbart jobba med factories och produkter genom abstrakta typer:
+         - AbstractFactory
+         - AbstractPRoduct
+
+        Fördel: du kan skicka in vilken factory o product (underklass) som du vill utan att behöva ändra klientkoden
+
+        Du kan t.ex vid uppstart bestämma vilken factory att använda
+             */
+        class Client
+        {
+            public void Main()
+            {
+                // The client code can work with any concrete factory class.
+                Console.WriteLine("Client: Testing client code with the first factory type...");
+                ClientMethod(new ConcreteFactory1());
+                Console.WriteLine();
+
+                Console.WriteLine("Client: Testing the same client code with the second factory type...");
+                ClientMethod(new ConcreteFactory2());
+            }
+
+            public void ClientMethod(IAbstractFactory factory)
+            {
+                var productA = factory.CreateProductA();
+                var productB = factory.CreateProductB();
+
+                Console.WriteLine(productB.UsefulFunctionB());
+                Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+            }
+        }
+    }
+}
---
+++ AbstractFactory/Demo2.cs
@@ -0,0 +1,183 @@
+﻿/*
+ ABSTRACT FACTORY 
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.AbstractFactory
+{
+    [TestClass]
+    public class Demo2
+    {
+        static readonly List<string> _log = new List<string>();
+
+        [TestMethod]
+        public void Ex1()
+        {
+            new Client().Main();
+
+            CollectionAssert.AreEqual(new[] {
+
+                "Shuffling cards",
+                "You have 2 cards on your hand",
+                "The deck have 50 cards",
+
+                "Blandar korten",
+                "Din hand består av 2 kort",
+                "Leken har 50 kort",
+
+            }, _log);
+
+        }
+
+
+        interface IAbstractFactory
+        {
+            AbstractDeck CreateDeck();
+            AbstractPlayer CreatePlayer();
+        }
+
+        class SwedishCardFactory : IAbstractFactory
+        {
+            public AbstractDeck CreateDeck() => new SwedishDeck();
+            public AbstractPlayer CreatePlayer() => new SwedishPlayer();
+        }
+
+        class EnglishCardFactory : IAbstractFactory
+        {
+            public AbstractDeck CreateDeck() => new EnglishDeck();
+            public AbstractPlayer CreatePlayer() => new EnglishPlayer();
+        }
+
+        abstract class AbstractDeck
+        {
+            protected readonly Queue<Card> _cards;
+            public AbstractDeck()
+            {
+                _cards = new Queue<Card>();
+
+                _cards.Enqueue(new Card(8, Color.Diamonds));
+
+                for (int i = 0; i < 51; i++)
+                {
+                    _cards.Enqueue(new Card(1, Color.Spades));
+                }
+            }
+            public virtual void Shuffle()
+            {
+                // norwegian shuffle
+            }
+
+            internal Card PickTopCard()
+            {
+                return _cards.Dequeue();
+            }
+
+            public abstract void Describe();
+        }
+
+        class SwedishDeck : AbstractDeck
+        {
+            public override void Describe()
+            {
+                _log.Add($"Leken har {_cards.Count} kort");
+            }
+
+            public override void Shuffle()
+            {
+                _log.Add("Blandar korten");
+                base.Shuffle();
+            }
+        }
+
+        class EnglishDeck : AbstractDeck
+        {
+            public override void Describe()
+            {
+                _log.Add($"The deck have {_cards.Count} cards");
+            }
+
+            public override void Shuffle()
+            {
+                _log.Add("Shuffling cards");
+                base.Shuffle();
+            }
+        }
+
+        abstract class AbstractPlayer
+        {
+            protected Queue<Card> _hand = new Queue<Card>();
+
+            public void PickOneCardFrom(AbstractDeck deck)
+            {
+                Card card = deck.PickTopCard();
+                _hand.Enqueue(card);
+            }
+
+            public abstract void DescribeHand();
+
+            public IEnumerable<Card> Hand => _hand;
+        }
+
+        class SwedishPlayer : AbstractPlayer
+        {
+            public override void DescribeHand() 
+            {
+                _log.Add($"Din hand består av {_hand.Count} kort");
+            }
+        }
+
+        class EnglishPlayer : AbstractPlayer
+        {
+            public override void DescribeHand()
+            {
+                _log.Add($"You have {_hand.Count} cards on your hand");
+            }
+        }
+
+        class Card
+        {
+
+            public Card(int rank, Color diamonds)
+            {
+                if (rank < 1 || rank > 13)
+                    throw new ArgumentException();
+
+                Rank = rank;
+                Diamonds = diamonds;
+            }
+
+            public int Rank { get; }
+            public Color Diamonds { get; }
+        }
+
+        enum Color
+        {
+            Spades, Hearts, Clubs, Diamonds
+        }
+
+
+        class Client
+        {
+            public void Main()
+            {
+                ClientMethod(new EnglishCardFactory());
+                ClientMethod(new SwedishCardFactory());
+            }
+
+            void ClientMethod(IAbstractFactory factory)
+            {
+                var player = factory.CreatePlayer();
+                var deck = factory.CreateDeck();
+
+                deck.Shuffle();
+                player.PickOneCardFrom(deck);
+                player.PickOneCardFrom(deck);
+
+                player.DescribeHand();
+                deck.Describe();
+            }
+        }
+    }
+}
