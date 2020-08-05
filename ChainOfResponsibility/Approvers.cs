6d95e7f54867abd093d6649c11dde05b5bc32ed2
Oscar Olsson <happybits@gmail.com>
2020-08-05 11:50:48 +0200

    -

--- ChainOfResponsibility/Approvers.cs
+++ ChainOfResponsibility/Approvers.cs
@@ -36,41 +36,52 @@ namespace DesignPatterns.ChainOfResponsibility
                Setup Chain of Responsibility
                All are Approvers
             */
+
             Approver larry = new Director();   // har minst makt
             Approver sam = new VicePresident();
             Approver tammy = new President();
 
-            /*
-            Setup chain
-            - Larry has Sam as successor (efterträdare). Larry has the least power.
-            - Sam has Tammy as successor
-            */
-            larry.SetSuccessor(sam); 
-            sam.SetSuccessor(tammy); 
+            var client = new Client();
 
-            // Small purchase is okay for the Director Larry
+            client.SetupChain(larry, sam, tammy);
 
-            Purchase p = new Purchase(1, 50, "Assets"); // Amount 50, Request number 1
-            larry.ProcessRequest(p);
-            Assert.AreEqual($"Director approved request 1", _logger.Last());
+            client.ProcessPurchase(new Purchase(1, 50, "Assets"));
+            client.ProcessPurchase(new Purchase(2, 12000, "Project X"));
+            client.ProcessPurchase(new Purchase(3, 75000, "Project Y"));
+            client.ProcessPurchase(new Purchase(4, 2000000, "Project Z"));
 
-            // Director can't handle => VicePresident cant handle => President can handle
+            CollectionAssert.AreEqual(new[] {
 
-            p = new Purchase(2, 75000, "Project X"); // Amount 75000, Request number 2
-            larry.ProcessRequest(p);
-            Assert.AreEqual($"President approved request 2", _logger.Last());
+            "Director approved request 1",
+            "VicePresident approved request 2",
+            "President approved request 3",
+            "No one can handle request :(",
 
-            // Too expensive! No one can handle!
-
-            p = new Purchase(3, 2000000, "Project Y"); // Amount 2000000, Request number 3
-            larry.ProcessRequest(p);
-            Assert.AreEqual($"No one can handle request :(", _logger.Last());
-
-            Assert.AreEqual(3, _logger.Count);
+            },
+            _logger);
 
         }
 
         #region solution
+
+        class Client
+        {
+            private Approver _firstApprover;
+
+            internal void ProcessPurchase(Purchase purchase)
+            {
+                _firstApprover.ProcessRequest(purchase);
+            }
+
+            internal void SetupChain(params Approver[] approvers)
+            {
+                for (int i = 0; i < approvers.Length-1; i++)
+                {
+                    approvers[i].SetSuccessor(approvers[i + 1]);
+                }
+                _firstApprover = approvers[0];
+            }
+        }
 
         // The 'Handler' abstract class
         // All Approvers has a "successor" and should be able to process a request
