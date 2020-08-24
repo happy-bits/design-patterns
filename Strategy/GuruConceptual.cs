11fb96e7c14e77c89d08478234308ea4c9e64a07
Oscar Olsson <happybits@gmail.com>
2020-08-24 08:13:08 +0200

    -

--- Strategy/GuruConceptual.cs
+++ Strategy/GuruConceptual.cs
@@ -13,10 +13,6 @@ namespace DesignPatterns.Strategy
             /*
                Välj en konkret strategi och skicka in den i "Context". 
                Clienten vet skillnad på strategierna
-              
-               The client code picks a concrete strategy and passes it to the
-               context. The client should be aware of the differences between
-               strategies in order to make the right choice.
             */
             var context = new Context();
 
@@ -31,15 +27,12 @@ namespace DesignPatterns.Strategy
             context.DoSomeBusinessLogic();
         }
 
-        // The Context defines the interface of interest to clients.
         class Context
         {
             /*
-                "Context" känner inte till de konkreta strategierna (bra)
+                "Context" håller en referens till ett strategiobjekt
 
-                The Context maintains a reference to one of the Strategy objects. The
-                Context does not know the concrete class of a strategy. It should
-                work with all strategies via the Strategy interface.
+                "Context" känner inte till de konkreta strategierna (bra) utan jobbar med strategins interface
             */
             private IStrategy _strategy;
 
@@ -47,28 +40,26 @@ namespace DesignPatterns.Strategy
             {
             }
 
-            // Usually, the Context accepts a strategy through the constructor, but
-            // also provides a setter to change it at runtime.
+            // Vanligt att strategin kan sättas i konstrutorn men att det också finns en setter
             public Context(IStrategy strategy)
             {
                 _strategy = strategy;
             }
 
-            // Usually, the Context allows replacing a Strategy object at runtime.
+            // Vi kan ändra strategin i runtime
             public void SetStrategy(IStrategy strategy)
             {
                 _strategy = strategy;
             }
 
-            // The Context delegates some work to the Strategy object instead of
-            // implementing multiple versions of the algorithm on its own.
+            // Istället för att context själv implementerar olika varianter av strategier så delegeras det jobbet
             public void DoSomeBusinessLogic()
             {
                 Console.WriteLine("Context: Sorting data using the strategy (not sure how it'll do it)");
                 var result = _strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
 
                 string resultStr = string.Empty;
-                foreach (var element in result as List<string>)
+                foreach (var element in result)
                 {
                     resultStr += element + ",";
                 }
@@ -77,22 +68,14 @@ namespace DesignPatterns.Strategy
             }
         }
 
-        // The Strategy interface declares operations common to all supported
-        // versions of some algorithm.
-        //
-        // The Context uses this interface to call the algorithm defined by Concrete
-        // Strategies.
         public interface IStrategy
         {
-            object DoAlgorithm(object data);
+            List<string> DoAlgorithm(object data);
         }
 
-        // Concrete Strategies implement the algorithm while following the base
-        // Strategy interface. The interface makes them interchangeable in the
-        // Context.
         class ConcreteStrategyA : IStrategy
         {
-            public object DoAlgorithm(object data)
+            public List<string> DoAlgorithm(object data)
             {
                 var list = data as List<string>;
                 list.Sort();
@@ -103,7 +86,7 @@ namespace DesignPatterns.Strategy
 
         class ConcreteStrategyB : IStrategy
         {
-            public object DoAlgorithm(object data)
+            public List<string> DoAlgorithm(object data)
             {
                 var list = data as List<string>;
                 list.Sort();
