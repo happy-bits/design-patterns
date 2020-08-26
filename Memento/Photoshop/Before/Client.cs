e1f79d2ed34dabe6ad3db46c9a9d46cbb621f6f6
Oscar Olsson <happybits@gmail.com>
2020-08-26 11:37:07 +0200

    Memento - Before-version

--- Memento/Photoshop/Before/Client.cs
+++ Memento/Photoshop/Before/Client.cs
@@ -1,6 +1,5 @@
 ﻿
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,48 +9,47 @@ namespace DesignPatterns.Memento.Photoshop.Before
     {
         public void DoStuff()
         {
-            throw new NotImplementedException();
             var circle1 = new Circle(0, 0, 2);
             var circle2 = new Circle(5, 5, 10);
             var circle3 = new Circle(200, 300, 100);
             var dot1 = new Dot(77, 88);
 
             Originator originator = new Originator();
-            Caretaker caretaker = new Caretaker(originator);
 
             originator.AddGraphic(circle1);
             Assert.AreEqual("Circle(0,0,2)", originator.StateInfo);
-            caretaker.Backup();
+            originator.Backup();
 
             originator.AddGraphic(circle2);
             Assert.AreEqual("Circle(0,0,2) Circle(5,5,10)", originator.StateInfo);
-            caretaker.Backup();
+            originator.Backup();
 
             originator.AddGraphic(circle3);
             Assert.AreEqual("Circle(0,0,2) Circle(5,5,10) Circle(200,300,100)", originator.StateInfo);
-            caretaker.Backup();
+            originator.Backup();
 
             originator.AddGraphic(dot1);
             Assert.AreEqual("Circle(0,0,2) Circle(5,5,10) Circle(200,300,100) Dot(77,88)", originator.StateInfo);
-            caretaker.Backup();
+            originator.Backup();
 
             originator.RemoveAllCircles();
             Assert.AreEqual("Dot(77,88)", originator.StateInfo);
 
             // Undoing
 
-            caretaker.Undo();
+            originator.Undo();
             Assert.AreEqual("Circle(0,0,2) Circle(5,5,10) Circle(200,300,100) Dot(77,88)", originator.StateInfo);
 
-            caretaker.Undo();
-            caretaker.Undo();
-            caretaker.Undo();
+            originator.Undo();
+            originator.Undo();
+            originator.Undo();
             Assert.AreEqual("Circle(0,0,2)", originator.StateInfo);
         }
 
         class Originator
         {
             private List<Graphic> _state;
+            private readonly Stack<List<Graphic>> _backup = new Stack<List<Graphic>>();
 
             public Originator(params Graphic[] state)
             {
@@ -62,83 +60,22 @@ namespace DesignPatterns.Memento.Photoshop.Before
             {
                 _state.Add(graphic);
             }
-
-            public IMemento CreateMemento()
-            {
-                return new ConcreteMemento(_state);
-            }
-
-            public void Restore(IMemento memento)
-            {
-                if (!(memento is ConcreteMemento))
-                {
-                    throw new Exception("Unknown memento class " + memento.ToString());
-                }
-
-                _state = memento.GetState().ToList();
-            }
-
+            
             public void RemoveAllCircles() => _state.RemoveAll(g => g is Circle);
-
-            public string StateInfo => string.Join(" ", _state.Select(s => s.ToString()));
-
-        }
-
-        public interface IMemento
-        {
-            List<Graphic> GetState();
-        }
-
-        class ConcreteMemento : IMemento
-        {
-            private readonly List<Graphic> _state;
-
-            public ConcreteMemento(IEnumerable<Graphic> state)
-            {
-                _state = state.ToList(); // måste ha "ToList" här
-            }
-
-            public List<Graphic> GetState() => _state;
-        }
-
-        class Caretaker
-        {
-            private readonly List<IMemento> _mementos = new List<IMemento>();
-
-            private readonly Originator _originator = null;
-
-            public Caretaker(Originator originator)
-            {
-                _originator = originator;
-            }
 
             public void Backup()
             {
-                _mementos.Add(_originator.CreateMemento());
+                var clonedState = _state.ToList();
+                _backup.Push(clonedState);
             }
 
             public void Undo()
             {
-                if (_mementos.Count == 0)
-                {
-                    return;
-                }
-
-                var memento = _mementos.Last();
-                _mementos.Remove(memento);
-
-                // Funkar denna try-catch?
-
-                try
-                {
-                    _originator.Restore(memento);
-                }
-                catch (Exception)
-                {
-                    Undo();
-                }
+                _state = _backup.Pop();
             }
-        }
 
+            public string StateInfo => string.Join(" ", _state.Select(s => s.ToString()));
+
+        }
     }
 }
