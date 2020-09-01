2cfa4afed819b98879781ac481d7291e9eb7f996
Oscar Olsson <happybits@gmail.com>
2020-09-01 09:43:47 +0200

    -

--- ChainOfResponsibility/Requests/Before/Client.cs
+++ ChainOfResponsibility/Requests/Before/Client.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DesignPatterns.Template.Requests.Before
+﻿namespace DesignPatterns.Template.Requests.Before
 {
     class Client : IClient
     {
-        public Response Authenticate(Request request)
-        {
-            throw new NotImplementedException();
-        }
 
         public void SetupChain()
         {
-            throw new NotImplementedException();
         }
+
+        public Response Authenticate(Request request)
+        {
+            if (request.User.IsInRole("Administrator"))
+                return new Response(true);
+
+            if (request.User.Name=="bobo")
+                return new Response(true);
+
+            return new Response(false);
+        }
+
+
+
     }
 }
+
