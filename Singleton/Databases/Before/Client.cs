7ba32fc69f8765b644534715fb50cdd0ec783119
Oscar Olsson <happybits@gmail.com>
2020-09-09 15:01:20 +0200

    -

--- Singleton/Databases/Before/Client.cs
+++ Singleton/Databases/Before/Client.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DesignPatterns.Singleton.Databases.Before
 {
@@ -7,7 +10,32 @@ namespace DesignPatterns.Singleton.Databases.Before
     {
         public void DoStuff()
         {
-            throw new NotImplementedException();
+            var x = new Database();
+            x.Query("SELECT * ...");
+
+            // some code
+
+            var y = new Database();
+            y.Query("SELECT Customer ...");
+
+            CollectionAssert.AreEqual(new[] {
+                //"SELECT * ...",
+                "SELECT Customer ..."   // "y" innehåller bara den ena SQL-koden eftersom "x" och "y" är olika objekt
+            }, y.Commands.ToArray());
         }
     }
+
+    public class Database
+    {
+
+        private readonly ConcurrentQueue<string> _commands = new ConcurrentQueue<string>();
+
+        public IEnumerable<string> Commands => _commands;
+
+        public void Query(string sql)
+        {
+            _commands.Enqueue(sql);
+        }
+    }
+
 }
--- Singleton/Databases/Tests.cs
+++ Singleton/Databases/Tests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using static DesignPatterns.TestUtilities;
 
 namespace DesignPatterns.Singleton.Databases
 {
@@ -8,7 +7,7 @@ namespace DesignPatterns.Singleton.Databases
     public class Tests
     {
         private IEnumerable<IClient> AllClients() => new IClient[] { 
-            //new Before.Client(), 
+            new Before.Client(), 
             new After.Client(), 
         };
 
